88d98f4dcd3e7c1e8978c5b884fde8af0d4fa947
Batyrzhan <batyrzhantogaev2002@icloud.com>
2024-09-30 18:05:40 +0500

    two

--- main/views.py
+++ main/views.py
@@ -10,7 +10,6 @@ from django.core.paginator import Paginator
 from .forms import UserRegisterForm, ArtworkUploadForm, CommentForm
 from .models import Artwork
 
-
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -24,30 +23,26 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
 
-
 @login_required
 def profile(request):
     return render(request, 'profile.html')
 
-
 class ArtworkListView(ListView):
     model = Artwork
-    template_name = 'home.html'  # Используемый шаблон
+    template_name = 'home.html'
     context_object_name = 'artworks'
-    paginate_by = 10  # Пагинация на 10 работ
-    ordering = ['-created_at']  # Сортировка по дате создания
-
+    paginate_by = 10
+    ordering = ['-created_at']
 
 class ArtworkCreateView(LoginRequiredMixin, CreateView):
     model = Artwork
     form_class = ArtworkUploadForm
     template_name = 'artwork_form.html'
-    success_url = reverse_lazy('home')  # После загрузки возвращаемся на главную страницу
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
 
 class ArtworkDetailView(DetailView):
     model = Artwork
@@ -57,11 +52,12 @@ class ArtworkDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         comments_list = self.get_object().comments.all()
-        paginator = Paginator(comments_list, 5)  # Пагинация по 5 комментариев на страницу
+        paginator = Paginator(comments_list, 5)
         page = self.request.GET.get('page')
         comments = paginator.get_page(page)
         context['comments'] = comments
         context['form'] = CommentForm()
+        context['artwork_id'] = self.get_object().id  # Добавьте эту строку
         return context
 
     def post(self, request, *args, **kwargs):
@@ -72,7 +68,6 @@ class ArtworkDetailView(DetailView):
             comment.artwork = artwork
             comment.author = request.user
             comment.save()
-            return redirect('artwork-detail', pk=artwork.pk)
-        # В случае ошибки, возвращаем контекст с формой и сообщениями об ошибках
+            return redirect('artwork-detail', id=artwork.id)
         context = self.get_context_data(artwork=artwork, form=form)
         return self.render_to_response(context)
